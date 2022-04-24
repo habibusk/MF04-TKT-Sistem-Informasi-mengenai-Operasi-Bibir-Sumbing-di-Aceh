dfc79ddcb9e99f11ec7fd6c55bc4b09c3d1ae8f1
Ahmad Zakiyyul Fikri <zakiyyul@mhs.unsyiah.ac.id>
2022-04-24 17:27:15 +0700

    Menambahkan tampilan contact us

---
+++ contactus.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/contac.css">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>CONTAC US | FullSenyum</title>
+</head>
+
+<body>
+    <!-- Navbar menu navigasi -->
+    <div class="navbar">
+        <div class="logo">
+            <img src="img/logo.png" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+                <a href="index.php">
+                    <li>Beranda</li>
+                </a>
+                <a href="kasus.php">
+                    <li>Kasus</li>
+                </a>
+                <a href="donasi.php">
+                    <li>Donasi</li>
+                </a>
+                <a href="">
+                    <li>Hubungi Kami</li>
+                </a>
+            </ul>
+        </div>
+        <div class="lout">
+            <a href=""><img src="img/out.png" alt="" width="202px" height="60px"></a>
+        </div>
+    </div>
+    <!-- end navbar menu navigasi -->
+
+
+
+    <!-- start konten -->
+    <div class="konten">
+        <div class="kontak">
+            <h6>KONTAK KAMI</h6>
+            <h3>Ada Yang Bisa Kami Bantu ?</h3>
+            <p>Isi Form Di Samping Untuk Mengirimkan Pesan Ke Kami</p>
+            <div class="narahubung">
+                <ul>
+                    <li><img src="img/FACE.png" alt="">&nbsp&nbsp Fullsenyumindonesia</li>
+                    <li><img src="img/MAIL.png" alt="">&nbsp fullysmile@mail.com</li>
+                    <li><img src="img/INSTA.png" alt="">&nbsp FullSenyum.id</li>
+                    <li><img src="img/PHONE.png" alt="">&nbsp 62 8123 456</li>
+                </ul>
+            </div>
+        </div>
+        <div class="form">
+            <div class="kotak">
+                <form action="">
+                    <ul>
+                        <li>
+                            <label for="nama">Nama </label>
+                            <input type="text" class="form-control" size="50" name="nama">
+                        </li>
+                        <li>
+                            <label for="mail">Email</label>
+                            <input type="text" class="form-control" size="50" name="mail">
+                        </li>
+                        <li>
+                            <label for="sub">Subject</label>
+                            <input type="text" class="form-control" size="50" name="sub">
+                        </li>
+                        <li>
+                            <label for="pesan">Message</label>
+                            <textarea name="" class="form-control" id="" cols="30" rows="5"></textarea>
+                        </li>
+                    </ul>
+                    <div class="submit">
+                        <button type="submit" name="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- end konten -->
+
+
+
+    <!-- footer -->
+    <div class="foot">
+        <div class="icon-logo">
+            <img src="img/logo putih.png" alt="">
+        </div>
+        <div class="alamat">
+            <p>Jl, Kan dulu Kampung apa,<br> Kota Apa. Provinsi apa. Negara Apa</p>
+        </div>
+        <div class="menu-foot">
+            <h3>Main Menu</h3>
+            <ul>
+                <li>Beranda</li>
+                <li>Kasus</li>
+                <li>Donasi</li>
+                <li>Hubungi Kami</li>
+            </ul>
+        </div>
+        <div class="sosmed">
+            <h3>Our Social Media</h3>
+            <ul>
+                <li><img src="img/twt.png" alt=""> Twitter</li>
+                <li><img src="img/ig.png" alt=""> Instagram</li>
+                <li><img src="img/fb.png" alt=""> Facebook</li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html
