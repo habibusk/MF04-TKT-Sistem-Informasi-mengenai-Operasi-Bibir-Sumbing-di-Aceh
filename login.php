f09378479ae23aa1b27e286f1fcebc2949742708
Ahmad Zakiyyul Fikri <zakiyyul@mhs.unsyiah.ac.id>
2022-05-14 16:25:52 +0700

    Menambahkan halaman untuk login

---
+++ login.php
@@ -0,0 +1,40 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <form class="form-signin">
+      <div class="foto">
+        <img  src="img/logo.png" alt="" width="72" height="72">
+        
+      </div>
+      <h1 class="judul h3 mb-3 font-weight-normal">Full Senyum.id</h1>
+      <label for="inputEmail" class="label sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="" required autofocus>
+      <label for="inputPassword" class="label sr-only mt-3">Password</label>
+      <input type="password" id="inputPassword" class="form-control " placeholder="" required>
+      <div class="lupa checkbox mb-3">
+        <label>
+           <a href="">Lupa Password?</a>
+        </label>
+      </div>
+      <button class="tombol" name="submit" type="submit">Login</button>
+      <p class="bawah mt-5 mb-3 text-center">Belum Memiliki Akun? <a href=""> Daftar</a></p>
+    </form>
+  </body>
+</html>
