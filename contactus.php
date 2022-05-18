f441e187d01634320a6bb238974720299729167f
Hurryatul Aqif <aqif@mhs.unsyiah.ac.id>
2022-05-18 14:30:59 +0700

    memperbaiki halaman contact us

--- contactus.php
+++ contactus.php
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> main
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,7 +32,7 @@
                     <li>Donasi</li>
                 </a>
                 <a href="">
-                    <li>Hubungi Kami</li>
+                    <li>Contact Us</li>
                 </a>
             </ul>
         </div>
@@ -49,69 +42,7 @@
     </div>
     <!-- end navbar menu navigasi -->
 
-    <!-- start banner website -->
-    <div class="banner">
-        <div class="tagline">
-            <h>Ada yang Bisa Kami Bantu?</h>
-            <a href=""><img src="img/fbgry.png" alt="" width="10px" height="10px">fullsenyum.id</a>
-            <a href=""><img src="img/iggry.png" alt="" width="10px" height="10px">fullsenyum.id</a>
-            <a href=""><img src="img/emailgry.png" alt="" width="10px" height="10px">fullsenyum@gmail.com </a>
-            <a href=""><img src="img/phone.png" alt="" width="10px" height="10px">+62 8123 456</a>
-            <div class="join">
-            </div>
-        </div>
-        <div class="ilus">
-                        
-        </div>
-    </div>
-    <!-- end banner website -->
 
-    <!-- start konten -->
-    <div class="konten">
-        <h1>isi konten di sini!!</h1>
-    </div>
-    <!-- end konten -->
-
-
-
-    <!-- footer -->
-    <div class="foot">
-        <div class="icon-logo">
-            <img src="img/logo putih.png" alt="">
-        </div>
-        <div class="alamat">
-            <p>Jl, Kan dulu Kampung apa,<br> Kota Apa. Provinsi apa. Negara Apa</p>
-        </div>
-        <div class="menu-foot">
-            <h3>Main Menu</h3>
-            <ul>
-                <li>Beranda</li>
-                <li>Kasus</li>
-                <li>Donasi</li>
-                <li>Hubungi Kami</li>
-            </ul>
-        </div>
-        <div class="sosmed">
-            <h3>Our Social Media</h3>
-            <ul>
-                <li><img src="img/twt.png" alt=""> Twitter</li>
-                <li><img src="img/ig.png" alt=""> Instagram</li>
-                <li><img src="img/fb.png" alt=""> Facebook</li>
-            </ul>
-        </div>
-    </div>
-</body>
-
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> 2004111010035
->>>>>>> main
-=======
->>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
 
@@ -119,115 +50,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< Updated upstream
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/contactus.css">
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
     <link rel="stylesheet" href="css/contactus.css">
-=======
+
     <link rel="stylesheet" href="css/contac.css">
->>>>>>> Stashed changes
-=======
     <link rel="stylesheet" href="css/contac.css">
->>>>>>> 2004111010035
->>>>>>> main
-=======
     <link rel="stylesheet" href="css/contac.css">
->>>>>>> Stashed changes
     <link rel="icon" type="image/x-icon" href="img/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<<<<<<< Updated upstream
-<<<<<<< HEAD
+
     <title>HOME | FullSenyum</title>
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
     <title>HOME | FullSenyum</title>
-=======
+
     <title>CONTAC US | FullSenyum</title>
->>>>>>> Stashed changes
-=======
+
+
     <title>CONTAC US | FullSenyum</title>
->>>>>>> 2004111010035
->>>>>>> main
-=======
     <title>CONTAC US | FullSenyum</title>
->>>>>>> Stashed changes
 </head>
 
 <body>
-    <!-- Navbar menu navigasi -->
-    <div class="navbar">
-        <div class="logo">
-            <img src="img/logo.png" alt="">
-        </div>
-        <div class="menu">
-            <ul>
-                <a href="index.php">
-                    <li>Beranda</li>
-                </a>
-                <a href="kasus.php">
-                    <li>Kasus</li>
-                </a>
-                <a href="donasi.php">
-                    <li>Donasi</li>
-                </a>
-                <a href="#">
-                    <li>Hubungi Kami</li>
-                </a>
-            </ul>
-        </div>
-        <div class="lout">
-            <a href=""><img src="img/out.png" alt="" width="202px" height="60px"></a>
-        </div>
-    </div>
-    <!-- end navbar menu navigasi -->
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
->>>>>>> main
-    <!-- start banner website -->
-    <div class="banner">
-        <div class="tagline">
-            <h>Ada yang Bisa Kami Bantu?</h>
-            <a href=""><img src="img/ig2.png" alt="" width="40px" height="40px">fullsenyum.id</a>
-            <a href=""><img src="img/gmail.png" alt="" width="40px" height="40px">fullsenyum@gmail.com</a>
-            <a href=""><img src="img/wa2.png" alt="" width="40px" height="40px">0852-XXXX-XXXX</a>
-            <div class="join">
-            </div>
-        </div>
-        <div class="ilus">
-                        
-        </div>
-    </div>
-    <!-- end banner website -->
 
-    <!-- start konten -->
-    <div class="konten">
-        <h1>isi konten di sini!!</h1>
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> 2004111010035
 
 
     <!-- start konten -->
     <div class="konten">
-=======
-
-
-    <!-- start konten -->
-    <div class="konten">
->>>>>>> Stashed changes
         <div class="kontak">
             <h6>KONTAK KAMI</h6>
             <h3>Ada Yang Bisa Kami Bantu ?</h3>
@@ -268,14 +119,6 @@
                 </form>
             </div>
         </div>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 2004111010035
->>>>>>> main
-=======
->>>>>>> Stashed changes
     </div>
     <!-- end konten -->
 
@@ -308,23 +151,3 @@
         </div>
     </div>
 </body>
-
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> main
-</html>
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
->>>>>>> main
-</html>
-=======
-</html
->>>>>>> Stashed changes
-=======
-</html
->>>>>>> 2004111010035
->>>>>>> main
-=======
-</html
->>>>>>> Stashed changes
