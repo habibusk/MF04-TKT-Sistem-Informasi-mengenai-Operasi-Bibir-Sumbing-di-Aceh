ddb568e09eecc98eb9bdda1df15266b046feddc8
Muhammad Hakim Windra <windra@mhs.unsyiah.ac.id>
2022-05-20 02:54:13 +0700

    Membuat halaman serta fitur register

---
+++ register.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>MASUK | FullSenyum</title>
+
+    <!-- Custom styles for this template -->
+    <link href="css/register.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <form class="form-signin">
+      <div class="foto">
+        <img  src="img/logo.png" alt="" width="72" height="72">
+        
+      </div>
+      <h1 class="judul h3 mb-3 font-weight-normal">Full Senyum.id</h1>
+      <label for="inputName" class="label sr-only">Nama</label>
+      <input type="name" id="inputName" class="form-control" placeholder="" required autofocus>
+      <label for="inputUser" class="label sr-only mt-3">Sebagai</label>
+      <input type="user" id="inputUser" class="form-control" placeholder="" required autofocus>
+      <label for="inputEmail" class="label sr-only mt-3">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="" required autofocus>
+      <label for="inputPassword" class="label sr-only mt-3">Password</label>
+      <input type="password" id="inputPassword" class="form-control " placeholder="" required>
+      <label for="inputPassword" class="label sr-only mt-3">Konfirmasi Password</label>
+      <input type="password" id="inputPassword" class="form-control " placeholder="" required>
+      
+      
+      <button class="tombol mt-4" name="submit" type="submit">Login</button>
+      <p class="bawah mt-3 mb-3 text-center">Sudah Memiliki Akun? <a href=""> Login</a></p>
+    </form>
+  </body>
+</html>
